2a4e931e5a587f2b7aed6ed297678b6915b5222c
Rushy Panchal <panchal.rushy@gmail.com>
2014-10-08 13:18:37 -0400

    Moved Back to sub-folder

--- __init__.py
+++ __init__.py
@@ -1,20 +1,4 @@
-# pysqlite.py
-# Rushy Panchal
-# Copyright 2014
-# Provides an interface for SQLite databases, using the sqlite3 module
+# Allow imports to the package
+# Rushy Panchals
 
-### Imports
-
-from database import *
-from table import *
-from cursor import *
-from sqlstring import *
-
-### Constants
-
-ALL = "*"
-
-### Meta information
-
-__author__ = "Rushy Panchal"
-__version__ = 0.2
+from wire import *
